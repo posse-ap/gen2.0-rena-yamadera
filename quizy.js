15a48f64371ad3cd878732c2059c37da36afa2df
Rena Yamadera <renapi200i@i.softbank.jp>
2021-07-19 10:43:58 +0900

    タイトルの下線、中央揃え

--- quizy.js
+++ quizy.js
@@ -1,5 +1,12 @@
 'user strict';
-document.getElementById('correct').addEventListener('click', () =>{
-    document.getElementById('correct').style.background = "blue";
-    document.getElementById('correct').style.color = "white"
-});
+
+    document.getElementById('correct').addEventListener('click', () =>{
+        document.getElementById('correct').style.background= "#287dff" ;
+        document.getElementById('correct').style.color = "white";
+    })
+
+
+document.getElementById('wrong1').addEventListener('click', () =>{
+    document.getElementById('wrong1').style.background= "red";
+})
+
