43c14d2df7251810eaf55f490fe7b1054b2d70f1
Rena Yamadera <renapi200i@i.softbank.jp>
2021-07-19 11:42:34 +0900

    正解時のボックスの追加、選択肢の中央揃え

--- quizy.js
+++ quizy.js
@@ -3,10 +3,22 @@
     document.getElementById('correct').addEventListener('click', () =>{
         document.getElementById('correct').style.background= "#287dff" ;
         document.getElementById('correct').style.color = "white";
+        })
+    
+    document.getElementById('wrong1').addEventListener('click', () =>{
+        document.getElementById('wrong1').style.background= "#ff5128";
+        document.getElementById('wrong1').style.color = "#ffffff"
     })
 
+    document.getElementById('wrong2').addEventListener('click', () =>{
+        document.getElementById('wrong2').style.background= "#ff5128";
+        document.getElementById('wrong2').style.color = "#ffffff"
+    })
 
-document.getElementById('wrong1').addEventListener('click', () =>{
-    document.getElementById('wrong1').style.background= "red";
-})
+    const div = document.createElement('div');
+    div.classList.add('box');
 
+    div.addEventListener('click', () =>{
+        div.classList.toggle('box')
+    })
+    document.body.appendChild(div);
