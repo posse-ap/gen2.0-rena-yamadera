9c8bd423bf02a7f34f77de0e9f760c9593113162
Rena Yamadera <renapi200i@i.softbank.jp>
2021-07-21 15:51:32 +0900

    正解時の結果ボックス表示

--- quizy.js
+++ quizy.js
@@ -3,7 +3,8 @@
     document.getElementById('correct').addEventListener('click', () =>{
         document.getElementById('correct').style.background= "#287dff" ;
         document.getElementById('correct').style.color = "white";
-        })
+
+    })
     
     document.getElementById('wrong1').addEventListener('click', () =>{
         document.getElementById('wrong1').style.background= "#ff5128";
@@ -16,9 +17,42 @@
     })
 
     const div = document.createElement('div');
-    div.classList.add('box');
+    // // constの名前をdivからresultsに変更したi
+    // div.classList.add('box');
+    document.getElementById('div')
+    div.classList.add('box')
 
-    div.addEventListener('click', () =>{
-        div.classList.toggle('box')
+    document.getElementById("correct").addEventListener('click', () =>{
+        document.body.appendChild(div);
+        div.textContent = '正解';
     })
-    document.body.appendChild(div);
+    
+    // document.getElementById("correct").addEventListener = ('click, () 
+    //     document.getElementById('div')
+    //     div.classList.add('box')
+
+    // })
+     
+       
+        
+    // const div = document.createElement('div');
+    // constの名前をdivからresultsに変更したい
+    // div.classList.add('box');
+    // div.addEventListener('click', () =>{
+    //     if (document.getElementById('correct')){
+    //         div.textContent = '正解';
+    //     }
+    //     else(
+    //         div.textContent = '不正解'
+    //     )
+   
+        
+    // })
+    // document.body.appendChild(div);
+    // div.classList.toggle('box')
+    // if ('correct'){
+    //     div.textContent = '正解';
+    // } else{
+    //     div.textContent = '不正解'
+    // }
+     //    div.classList.toggle('box')
