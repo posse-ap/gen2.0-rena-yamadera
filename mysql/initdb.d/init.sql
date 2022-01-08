02525aec3dde3808014e6b1006dc17715c2fc724
Rena Yamadera <renapi200i@i.softbank.jp>
2022-01-08 22:22:14 +0900

    東京広島の2つのテーブル表示

--- mysql/initdb.d/init.sql
+++ mysql/initdb.d/init.sql
@@ -1,7 +1,8 @@
 USE quizy_database;
 CREATE TABLE big_questions(
- id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
- name varchar(255)
-);
+ `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
+ `name`  text CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 INSERT INTO big_questions(name) VALUES ('東京');
 INSERT INTO big_questions(name) VALUES ('広島');
